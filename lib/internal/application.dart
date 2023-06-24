2097e1436803e8c0a8376923530254bafec02b25
morozovilia <morozov.ilya.vl@gmail.com>
2023-06-24 19:38:09 +0400

    First commit

---
+++ lib/internal/application.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import '../presentation/home/home.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Need Help',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
